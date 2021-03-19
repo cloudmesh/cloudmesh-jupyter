5aa28974d829b5455e1a62dade54022471e9c17a
Gregor von Laszewski <laszewski@gmail.com>
2021-03-19 16:39:47 -0400

    update the jupyter command

--- cloudmesh/jupyter/Jupyter.py
+++ cloudmesh/jupyter/Jupyter.py
@@ -1,18 +1,31 @@
+import os
+from cloudmesh.common.Shell import Shell
+
 class Jupyter:
 
-    def __init__(self, *, user:str, host:str, port:int):
+    def __init__(self, user:str, host:str, port:int, directory:str):
         self.user = user
         self.port = port
         self.host = host
+        self.directory = directory or ""
 
     def start(self):
-        pass
+        command = f'ssh -t {self.user}@{self.host} "source .bash_profile; jupyter-lab {self.directory} --no-browser --port={self.port}"'
+        print(command)
+        os.system(command)
 
     def stop(self):
-        pass
+        ValueError("not yet implemented")
 
     def tunnel(self):
-        pass
+        command = f"ssh -N -f -L localhost:{self.port}:localhost:{self.port} {self.user}@{self.host}"
+        print (command)
+        os.system(command)
 
     def open(self):
-        pass
+        location = f"https://localhost:{self.port}/lab?"
+        print ("Open", location)
+        Shell.browser(location)
+
+
+    #  > /dev/null 2>&1 & echo $! > "dmr.pid"
--- cloudmesh/jupyter/command/jupyter.py
+++ cloudmesh/jupyter/command/jupyter.py
@@ -5,6 +5,7 @@ from cloudmesh.common.util import path_expand
 from pprint import pprint
 from cloudmesh.common.debug import VERBOSE
 
+from cloudmesh.jupyter.Jupyter import Jupyter
 
 class JupyterCommand(PluginCommand):
 
@@ -15,9 +16,10 @@ class JupyterCommand(PluginCommand):
         ::
 
           Usage:
-                jupyter start USER HOST PORT
+                jupyter start USER HOST PORT [DIR]
+                jupyter tunnel USER HOST PORT
                 jupyter stop
-                jupyter open
+                jupyter open PORT
 
           This command can start a jupyter notebook on a remote machine and
           use it in your browser.
@@ -26,13 +28,59 @@ class JupyterCommand(PluginCommand):
              USER   The username on the remote machine
              HOST   The hostname of the remote machine
              PORT   The port of the remote machine
+             DIR    The directory where the notebooks are located
+
+          Description:
+
+            Step 1: Setting up a .bash_profile file
+
+             If you have your python venv set up you need to add it to the
+             .bash_profile on your remote machine. A possible
+             profile file could look as follows:
+
+                if [ -f ~/.bash_aliases ]; then
+                     . ~/.bash_aliases
+                 fi
+
+                 export PATH=$HOME/ENV3/bin:$PATH
+                 source $HOME/ENV3/bin/activate
+
+            Step 2: Start the remote notebook server in a terminal
+
+                Note: After the start you will not be able to use that terminal
+
+                cms jupyter start USER HOST PORT
+
+            Step 3: Start the tunnel in a different terminal
+
+                cms jupyter start USER HOST PORT
+
+           Step 4: Open the jupyterlab
+
+                copy the https address that you see in the terminal for step 1
+                into your browser. It will look similar to
+
+                    http://127.0.0.1:8888/lab?token=1234...abcd
+
+                This does not  yet work
+                cms jupyter open PORT
 
         """
 
+        jupyter = Jupyter(arguments.USER, arguments.HOST, arguments.PORT, arguments.DIR)
+
         if arguments.start:
-            pass
+
+            jupyter.start()
+
+        if arguments.tunnel:
+
+            jupyter.tunnel()
+
         elif arguments.stop:
-            pass
+            jupyter.stop()
+
         elif arguments.open:
-            pass
+            jupyter.open()
+
         return ""
